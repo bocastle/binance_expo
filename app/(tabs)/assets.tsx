7d9bf4458ec331ff66e56e7218368e65100a7331
bocastle <bocastle1213@gmail.com>
2025-02-04 02:21:14 +0900

    탭 추가 이후 아이콘 변경 explore 파일 삭제 예정

---
+++ app/(tabs)/assets.tsx
@@ -0,0 +1,27 @@
+import { Image, StyleSheet } from "react-native";
+
+import ParallaxScrollView from "@/components/ParallaxScrollView";
+
+export default function TabAssetsScreen() {
+  return (
+    <ParallaxScrollView
+      headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
+      headerImage={
+        <Image
+          source={require("@/assets/images/partial-react-logo.png")}
+          style={styles.reactLogo}
+        />
+      }
+    ></ParallaxScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  reactLogo: {
+    height: 178,
+    width: 290,
+    bottom: 0,
+    left: 0,
+    position: "absolute",
+  },
+});
