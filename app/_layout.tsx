1869aa473dbe717244c0e49335bab3e354201183
bocastle <bocastle1213@gmail.com>
2025-02-05 01:14:41 +0900

    StatusBar 다크모드 확인

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -10,6 +10,7 @@ import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { StatusBar } from "expo-status-bar";
 import { useEffect } from "react";
+import { Platform } from "react-native";
 import "react-native-reanimated";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { RecoilRoot } from "recoil";
@@ -37,6 +38,8 @@ export default function RootLayout() {
   }
   const apiUrl = process.env.EXPO_PUBLIC_API_URL;
   console.log("apiUrl env 확인", apiUrl);
+  console.log("colorScheme", colorScheme);
+
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
@@ -46,7 +49,12 @@ export default function RootLayout() {
               <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
               <Stack.Screen name="+not-found" />
             </Stack>
-            <StatusBar style="auto" />
+            <StatusBar
+              style={Platform.select({
+                ios: colorScheme === "dark" ? "light" : "dark",
+                android: colorScheme ?? "auto",
+              })}
+            />
           </RecoilRoot>
         </QueryClientProvider>
       </ThemeProvider>
