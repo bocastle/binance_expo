a7708d3d6103460303e4cfde09baeffa326b07dc
bocastle <bocastle1213@gmail.com>
2025-02-05 00:31:00 +0900

    SafeAreaView 추가

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -11,6 +11,7 @@ import * as SplashScreen from "expo-splash-screen";
 import { StatusBar } from "expo-status-bar";
 import { useEffect } from "react";
 import "react-native-reanimated";
+import { SafeAreaView } from "react-native-safe-area-context";
 import { RecoilRoot } from "recoil";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
@@ -37,16 +38,18 @@ export default function RootLayout() {
   const apiUrl = process.env.EXPO_PUBLIC_API_URL;
   console.log("apiUrl env 확인", apiUrl);
   return (
-    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-      <QueryClientProvider client={queryClient}>
-        <RecoilRoot>
-          <Stack>
-            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-            <Stack.Screen name="+not-found" />
-          </Stack>
-          <StatusBar style="auto" />
-        </RecoilRoot>
-      </QueryClientProvider>
-    </ThemeProvider>
+    <SafeAreaView style={{ flex: 1 }}>
+      <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+        <QueryClientProvider client={queryClient}>
+          <RecoilRoot>
+            <Stack>
+              <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+              <Stack.Screen name="+not-found" />
+            </Stack>
+            <StatusBar style="auto" />
+          </RecoilRoot>
+        </QueryClientProvider>
+      </ThemeProvider>
+    </SafeAreaView>
   );
 }
