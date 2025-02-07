3a7340af6300f3b1e2f494969df8f0d88cf5e56c
bocastle <bocastle1213@gmail.com>
2025-02-07 22:17:23 +0900

    아이콘 추가

--- features/markets/components/SearchCoinPairs.tsx
+++ features/markets/components/SearchCoinPairs.tsx
@@ -1,6 +1,7 @@
+import Search from "@/assets/icon/search.svg";
+import ThreeDots from "@/assets/icon/three_dots.svg";
 import { useThemeColor } from "@/hooks/useThemeColor";
 import { Text, useColorScheme, View } from "react-native";
-
 const SearchCoinPairs = () => {
   const colorScheme = useColorScheme();
   const backgroundColor = useThemeColor(
@@ -16,6 +17,8 @@ const SearchCoinPairs = () => {
         paddingVertical: 8,
         paddingHorizontal: 16,
         backgroundColor: backgroundColor,
+        alignItems: "center",
+        gap: 5,
       }}
     >
       <View
@@ -23,11 +26,17 @@ const SearchCoinPairs = () => {
           flex: 1,
           padding: 5,
           borderRadius: 5,
-          borderWidth: 0.5,
-          borderColor: "#191a1999",
+          backgroundColor: "#cccacae9",
+          flexDirection: "row",
+          gap: 5,
+          alignItems: "center",
         }}
       >
+        <Search width={20} height={20} />
         <Text style={{ fontSize: 12, color: "gray" }}>Search Coin Paris</Text>
+      </View>
+      <View>
+        <ThreeDots width={15} height={15} />
       </View>
     </View>
   );
