5af50de3624e11f1009da85c543c7c10d536eff3
bocastle <bocastle1213@gmail.com>
2025-02-07 13:53:00 +0900

    정렬 아이콘 적용

--- features/markets/components/MarketListHeader.tsx
+++ features/markets/components/MarketListHeader.tsx
@@ -1,7 +1,6 @@
-import { IconSymbol } from "@/components/ui/IconSymbol";
+import ArrowDropDown from "@/assets/icon/arrow_drop.svg";
 import { useThemeColor } from "@/hooks/useThemeColor";
 import { Text, useColorScheme, View } from "react-native";
-
 const MarketListHeader = () => {
   const colorScheme = useColorScheme();
   const backgroundColor = useThemeColor(
@@ -25,26 +24,70 @@ const MarketListHeader = () => {
           <Text style={{ fontSize: 12, color: "gray" }}>Name</Text>
           <View style={{ flexDirection: "column" }}>
             <View>
-              <IconSymbol size={6} name="arrow.up" color={"#191919"} />
+              <ArrowDropDown
+                width={6}
+                height={6}
+                fill={"rgb(205, 203, 203)"}
+                style={{ transform: [{ rotate: "180deg" }] }}
+              />
             </View>
             <View>
-              <IconSymbol size={6} name="arrow.down" color={"#191919"} />
+              <ArrowDropDown width={6} height={6} fill={"rgb(205, 203, 203)"} />
             </View>
           </View>
         </View>
         <View>
           <Text style={{ fontSize: 12, color: "gray" }}>/</Text>
         </View>
-        <View style={{ flexDirection: "row" }}>
+        <View style={{ flexDirection: "row", alignItems: "center" }}>
           <Text style={{ fontSize: 12, color: "gray" }}>Vol</Text>
+          <View style={{ flexDirection: "column" }}>
+            <View>
+              <ArrowDropDown
+                width={6}
+                height={6}
+                fill={"rgb(205, 203, 203)"}
+                style={{ transform: [{ rotate: "180deg" }] }}
+              />
+            </View>
+            <View>
+              <ArrowDropDown width={6} height={6} fill={"rgb(205, 203, 203)"} />
+            </View>
+          </View>
         </View>
       </View>
       <View style={{ flexDirection: "row", gap: 20 }}>
-        <View>
+        <View style={{ flexDirection: "row", alignItems: "center" }}>
           <Text style={{ fontSize: 12, color: "gray" }}>Last Price</Text>
+          <View style={{ flexDirection: "column" }}>
+            <View>
+              <ArrowDropDown
+                width={6}
+                height={6}
+                fill={"rgb(205, 203, 203)"}
+                style={{ transform: [{ rotate: "180deg" }] }}
+              />
+            </View>
+            <View>
+              <ArrowDropDown width={6} height={6} fill={"rgb(205, 203, 203)"} />
+            </View>
+          </View>
         </View>
-        <View>
+        <View style={{ flexDirection: "row", alignItems: "center" }}>
           <Text style={{ fontSize: 12, color: "gray" }}>24h chg%</Text>
+          <View style={{ flexDirection: "column" }}>
+            <View>
+              <ArrowDropDown
+                width={6}
+                height={6}
+                fill={"rgb(205, 203, 203)"}
+                style={{ transform: [{ rotate: "180deg" }] }}
+              />
+            </View>
+            <View>
+              <ArrowDropDown width={6} height={6} fill={"rgb(205, 203, 203)"} />
+            </View>
+          </View>
         </View>
       </View>
     </View>
